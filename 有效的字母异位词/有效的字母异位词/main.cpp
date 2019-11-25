969f38512d54cf24617082148c0ee4e9d4785e7c
Info <zhenghailin@leoao.com>
2019-11-25 09:58:12 +0800

    有效的字母异味词

---
+++ 有效的字母异位词/有效的字母异位词/main.cpp
@@ -0,0 +1,47 @@
+//
+//  main.cpp
+//  有效的字母异位词
+//
+//  Created by Info on 2019/11/25.
+//  Copyright © 2019 Info. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <string>
+
+//给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+//
+//示例 1:
+//
+//输入: s = "anagram", t = "nagaram"
+//输出: true
+//示例 2:
+//
+//输入: s = "rat", t = "car"
+//输出: false
+//说明:
+//你可以假设字符串只包含小写字母。
+//
+//进阶:
+//如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+
+class Solution {
+public:
+    bool isAnagram(std::string s, std::string t) {
+        std::sort(s.begin(), s.end());
+        std::sort(t.begin(), t.end());
+        if (s == t) {
+            return true;
+        }
+        return false;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    Solution *solution = new Solution();
+    solution->isAnagram("rat", "car");
+    return 0;
+}
